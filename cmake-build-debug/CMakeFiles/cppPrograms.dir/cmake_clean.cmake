5bdb1cc79aeffe8fb661e110928cefd955299296
Lucas <lukinhassaito2013@hotmail.com>
2020-10-07 12:55:14 -0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/cppPrograms.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cppPrograms.dir/main.cpp.o"
+  "cppPrograms"
+  "cppPrograms.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cppPrograms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
